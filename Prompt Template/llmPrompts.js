fea6818ad8fd6af6bfbe74fe5d682520d2f4cbaf
tanvirtbf <tanver018765@gmail.com>
2025-03-18 14:53:15 +0600

    project setup for example 1

--- Prompt Template/llmPrompts.js
+++ Prompt Template/llmPrompts.js
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+// LLMS - Prompt Template
 
+import { OpenAI } from '@langchain/openai'
+import { PromptTemplate } from '@langchain/core/prompts'
+
+const llm = new OpenAI({})
 
