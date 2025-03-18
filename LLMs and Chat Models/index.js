e15aa8b514c02dfd003ad0b3dcec2791181a1449
tanvirtbf <tanver018765@gmail.com>
2025-03-18 11:50:59 +0600

    update

--- LLMs and Chat Models/index.js
+++ LLMs and Chat Models/index.js
@@ -1,1 +1,9 @@
-console.log('hi')
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+// LLM 
+import { OpenAI } from "@langchain/openai";
+const llm = new OpenAI({
+  model: "gpt-3.5-turbo-instruct",
+});
