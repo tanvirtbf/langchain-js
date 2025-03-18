ff2b6c4870a2dfd0dd6720f33a9e84bfa9ff9b94
tanvirtbf <tanver018765@gmail.com>
2025-03-18 15:09:21 +0600

    Prompt having One Input Variable Example Complete

--- Prompt Template/llmPrompts.js
+++ Prompt Template/llmPrompts.js
@@ -9,13 +9,26 @@ import { PromptTemplate } from '@langchain/core/prompts'
 const llm = new OpenAI({})
 
 
-// Example 1 - Prompt having No Input Variable
-const noInputPrompt = new PromptTemplate({
-  inputVariables : [],
-  template: "Tell me a trick of Javascript"
-})
-const formattedNoInputPrompt = await noInputPrompt.format()
-// console.log("No Input Prompt : ", formattedNoInputPrompt)
+// // Example 1 - Prompt having No Input Variable
+// const noInputPrompt = new PromptTemplate({
+//   inputVariables : [],
+//   template: "Tell me a trick of Javascript"
+// })
+// const formattedNoInputPrompt = await noInputPrompt.format()
+// // console.log("No Input Prompt : ", formattedNoInputPrompt)
 
-const response = await llm.invoke(formattedNoInputPrompt)
+// const response = await llm.invoke(formattedNoInputPrompt)
+// console.log("Response : ", response)
+
+// Example 2 - Prompt having One Input Variable
+const OneInputPrompt = new PromptTemplate({
+  inputVariables : ["language"],
+  template: "Tell me a trick of {language}"
+})
+const formattedOneInputPrompt = await OneInputPrompt.format({
+  language : "C++"
+})
+// console.log("No Input Prompt : ", formattedOneInputPrompt)
+
+const response = await llm.invoke(formattedOneInputPrompt)
 console.log("Response : ", response)
