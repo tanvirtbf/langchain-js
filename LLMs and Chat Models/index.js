472148ef78ac7005afb8a0a42b9464cb4843a409
tanvirtbf <tanver018765@gmail.com>
2025-03-18 12:33:25 +0600

    update

--- LLMs and Chat Models/index.js
+++ LLMs and Chat Models/index.js
@@ -3,11 +3,20 @@ dotenv.config()
 
 
 // LLM 
-import { OpenAI } from "@langchain/openai";
-const llm = new OpenAI({
-  model: "gpt-3.5-turbo-instruct",
-});
+// import { OpenAI } from "@langchain/openai";
+// const llm = new OpenAI({
+//   model: "gpt-3.5-turbo-instruct",
+// });
 
-const response = await llm.invoke("who is prime minister of bangladesh?")
-console.log("Response : ", response)
+// const response = await llm.invoke("who is prime minister of bangladesh?")
+// console.log("Response : ", response)
 
+
+// Chat Model 
+// import { ChatOpenAI } from "@langchain/openai";
+// const chat = new ChatOpenAI({});
+
+// Example 1 : 
+
+// const response = await chat.invoke('who is prime minister of banglades?')
+// console.log("Response : ",response)
