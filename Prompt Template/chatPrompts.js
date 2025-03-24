1b67c98e11de6723970b696a5b11c8bc1b994b4c
tanvirtbf <tanver018765@gmail.com>
2025-03-24 12:21:59 +0600

    language convert agent create

--- Prompt Template/chatPrompts.js
+++ Prompt Template/chatPrompts.js
@@ -1,4 +1,32 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-// Chat - Prompt Template
+// Chat - Prompt Templat
+
+import { ChatOpenAI } from '@langchain/openai'
+import { ChatPromptTemplate, PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate } from '@langchain/core/prompts'
+import { AIMessage, HumanMessage, SystemMessage } from '@langchain/core/messages'
+
+
+const chat = new ChatOpenAI({})
+
+// Example 1 - Message Prompt Template as Tuples
+const sys_template = "You are a helpful assistant that translates {input_language} to {output_language}."
+const human_template = "{text}"
+
+const chatPrompt = ChatPromptTemplate.fromMessages([
+  ["system", sys_template],
+  ["human", human_template]
+])
+
+const formattedChatPrompt = await chatPrompt.formatMessages({
+  input_language: "English",
+  output_language: "french",
+  text: "I am learing Langchain JS from GeekyShows YT"
+})
+
+// console.log('Formatted Chat Prompt : ', formattedChatPrompt)
+
+const response = await chat.invoke(formattedChatPrompt)
+console.log(response.content)
+
