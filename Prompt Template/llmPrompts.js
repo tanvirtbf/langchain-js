9bd6e24aa82d4de3bcddff355d5a9051786c8749
tanvirtbf <tanver018765@gmail.com>
2025-03-24 10:19:12 +0600

    update4

--- Prompt Template/llmPrompts.js
+++ Prompt Template/llmPrompts.js
@@ -9,16 +9,16 @@ import { PromptTemplate } from '@langchain/core/prompts'
 const llm = new OpenAI({})
 
 
-// // Example 1 - Prompt having No Input Variable
-// const noInputPrompt = new PromptTemplate({
-//   inputVariables : [],
-//   template: "Tell me a trick of Javascript"
-// })
-// const formattedNoInputPrompt = await noInputPrompt.format()
-// // console.log("No Input Prompt : ", formattedNoInputPrompt)
+// Example 1 - Prompt having No Input Variable
+const noInputPrompt = new PromptTemplate({
+  inputVariables : [],
+  template: "Tell me a trick of Javascript"
+})
+const formattedNoInputPrompt = await noInputPrompt.format()
+// console.log("No Input Prompt : ", formattedNoInputPrompt)
 
-// const response = await llm.invoke(formattedNoInputPrompt)
-// console.log("Response : ", response)
+const response = await llm.invoke(formattedNoInputPrompt)
+console.log("Response : ", response)
 
 // // Example 2 - Prompt having One Input Variable
 // const OneInputPrompt = new PromptTemplate({
@@ -33,16 +33,30 @@ const llm = new OpenAI({})
 // const response = await llm.invoke(formattedOneInputPrompt)
 // console.log("Response : ", response)
 
-// Example 3 - Prompt having Multiple Input Variable
-const MultiInputPrompt = new PromptTemplate({
-  inputVariables : ["language", "topic"],
-  template: "Tell me a trick of {language} , and topic is {topic}"
-})
-const formattedMultiInputPrompt = await MultiInputPrompt.format({
-  language : "Javascript",
-  topic: "Array"
-})
-// console.log("No Input Prompt : ", formattedMultiInputPrompt)
+// // Example 3 - Prompt having Multiple Input Variable
+// const MultiInputPrompt = new PromptTemplate({
+//   inputVariables : ["language", "topic"],
+//   template: "Tell me a trick of {language} , and topic is {topic}"
+// })
+// const formattedMultiInputPrompt = await MultiInputPrompt.format({
+//   language : "Javascript",
+//   topic: "Array"
+// })
+// // console.log("No Input Prompt : ", formattedMultiInputPrompt)
 
-const response = await llm.invoke(formattedMultiInputPrompt)
-console.log("Response : ", response)
+// const response = await llm.invoke(formattedMultiInputPrompt)
+// console.log("Response : ", response)
+
+// Example 4 - Prompt Template - No input variable manually
+// const template = "Tell me a trick of {language} from {topic}"
+// const promptTemplate = PromptTemplate.fromTemplate(template)
+// // console.log('Prompt Template: ', promptTemplate.inputVariables)
+
+// const formattedPromptTemplate = await promptTemplate.format({
+//   language: "python",
+//   topic: "function"
+// })
+// // console.log('Formatted Prompt Template : ', formattedPromptTemplate)
+
+// const response = await llm.invoke(formattedPromptTemplate)
+// console.log(response)
